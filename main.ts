837eff8ff0e6450442de330b099d4ddd8a151695
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-05-31 14:33:43 +0100

    parse html, log DOM object

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
 async function main() {
   const collectionPageResponse = await fetch(
     "https://www.gov.uk/government/collections/ministers-transparency-publications",
   );
   const collectionPageHtml = await collectionPageResponse.text();
-  console.log(collectionPageHtml);
+  const parser = new DOMParser();
+  const collectionPageDocument = parser.parseFromString(
+    collectionPageHtml,
+    "text/html",
+  );
+  console.log(collectionPageDocument);
 }
 
 main();
