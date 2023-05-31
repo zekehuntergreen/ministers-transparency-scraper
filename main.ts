81ae4f2ca4934bfa3e39064d2ab5c2439475da18
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-05-31 14:32:40 +0100

    fetch and log ministers transparency publications html

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-function main() {
-  console.log("Hello World!");
+async function main() {
+  const collectionPageResponse = await fetch(
+    "https://www.gov.uk/government/collections/ministers-transparency-publications",
+  );
+  const collectionPageHtml = await collectionPageResponse.text();
+  console.log(collectionPageHtml);
 }
 
 main();
